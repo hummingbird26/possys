70f306a6c6df9fbee61293145b31c9e38e008e52
smart-202-5 <smart-202-5@smart-202-5-PC>
2017-05-08 15:48:20 +0900

    2017.05.08 최홍락

--- lastproject/src/main/java/kr/or/possys/Menu_service/Menu_Dao.java
+++ lastproject/src/main/java/kr/or/possys/Menu_service/Menu_Dao.java
@@ -8,6 +8,8 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+
+
 @Repository
 public class Menu_Dao {
 
@@ -17,6 +19,30 @@ public class Menu_Dao {
 	//menu 입력요청
 	public int insertmenu(Menu menu){
 		System.out.println("menu_Controll.java <-- 실행확인 ");	
-		return sqlSessionTemplate.insert("kr.or.possys.menu_service.Menu_Mapper.insertmenu",menu);
+		return sqlSessionTemplate.insert("kr.or.possys.Menu_service.Menu_Mapper.insertmenu",menu);
 	}
+	//menu 리스트 요청
+	public int getmenucount(){
+		System.out.println("02_Menu_dao.java->>getmenucount 실행 ");
+		return sqlSessionTemplate.selectOne("kr.or.possys.Menu_service.Menu_Mapper.getmenucount");
+	}
+	public List<Menu> menulist(int currentPage, int pageRow){
+		System.out.println("03_menu_dao.java->>menulist 실행");
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		map.put("beginRow", (currentPage-1)*pageRow);
+		map.put("pageRow", pageRow);
+		return sqlSessionTemplate.selectList("kr.or.possys.Menu_service.Menu_Mapper.menulist",map);
+	}
+	//menu 수정폼 요청
+	public Menu menuview(String menu_id){
+		System.out.println("04_Menu_dao.java->>menuview 실행");
+		return sqlSessionTemplate.selectOne("kr.or.possys.Menu_service.Menu_Mapper.menuview",menu_id);
+	}
+	//menu 수정액션 요청
+	public int menumodify(Menu menu){
+		System.out.println("05_Menu_dao.java->>menumodify 실행");
+		return sqlSessionTemplate.update("kr.or.possys.Menu_service.Menu_Mapper.menumodify",menu);
+			
+		}
+	
 }
---
+++ lastproject/src/main/webapp/WEB-INF/views/zeus/menu_modify_view.jsp
@@ -0,0 +1,35 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=EUC-KR">
+<title>메뉴 화면</title>
+</head>
+<body>
+<h1>메뉴 - ${menu.menu_id} 수정 화면</h1>
+<br>
+
+<form id="addform" action="${pageContext.request.contextPath}/menu_modify" method="post">
+		
+		<div>메뉴코드 : <input name ="menu_id" id ="menu_id" type ="text" value="${menu.menu_id}" readonly="readonly"/></div>
+		<div>메뉴명: <input name ="menu_name" id ="menu_name" type ="text" value="${menu.menu_name}"/></div>
+		<div>상품카테고리: <input name ="menu_cate" id ="menu_cate" type ="text" value="${menu.menu_cate}"/></div>
+		<div>가격: <input name ="menu_price" id ="menu_price" type ="text" value="${menu.menu_price}"/></div>
+		<div>할인여부 : <input name ="menu_sprice" id ="menu_sprice" type ="text" value="${menu.menu_sprice}"/></div>
+		<div>칼로리: <input name ="menu_kcal" id ="menu_kcal" type ="text" value="${menu.menu_kcal}" readonly="readonly"/></div>
+		
+		
+
+<br>
+		<div>
+			<input type="submit" id="addmenu" value="수정">
+			<input type="reset" id="reset" value="초기화">
+			<a href="${pageContext.request.contextPath}/food_list">취소</a>
+			<a href="${pageContext.request.contextPath}/food_delete?food_id=${food.food_id}">삭제</a>
+		</div>
+	</form>
+
+</body>
+</html>
